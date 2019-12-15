876c44faac4f03c9121afe9d653e5cb8ba51ce47
alanmaranto <alanarostegui@hotmail.com>
2019-12-15 00:36:34 -0600

    Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './bootstrap.min.css';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      H
-    </div>
-  );
+class App extends Component {
+  render() { 
+    return ( 
+      <div className="container">
+        <Header 
+          titulo="Administrador Pacientes Veterinaria"
+        />
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
